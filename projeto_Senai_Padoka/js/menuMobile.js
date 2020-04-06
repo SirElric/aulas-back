d8e741eca77ed1fc8c608f3ef01ee3de9e7a7493
Erick <erick.matheusrs1003@gmail.com>
2020-04-06 20:42:58 -0300

    Adicionando Projeto Padoka

---
+++ projeto_Senai_Padoka/js/menuMobile.js
@@ -0,0 +1,8 @@
+"use strict";
+
+$(document).ready(function(){
+     $('.open-menu').click(function(){
+         $('.menu').toggleClass('active');
+         $('.open-menu').toggleClass('open');
+     })
+})
