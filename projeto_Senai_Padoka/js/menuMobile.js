968d47f9b0febfc7df5ebb043ef416be7d6399a0
Erick Rodrigues <erick.matheusrs1003@gmail.com>
2020-05-29 10:35:55 -0300

    deletando arquivos antigos

--- projeto_Senai_Padoka/js/menuMobile.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-$(document).ready(function(){
-     $('.open-menu').click(function(){
-         $('.menu').toggleClass('active');
-         $('.open-menu').toggleClass('open');
-     })
-})
