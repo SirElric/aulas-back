70aef56a1da8949d978021a9fd56949339e64ac3
Erick Matheus <erick.matheusrs1003@gmail.com>
2020-06-08 14:02:34 -0300

    atualizando css do prjeto

--- projeto_SENAI/functions/cardProductHome.php
+++ projeto_SENAI/functions/cardProductHome.php
@@ -9,10 +9,12 @@
         $demoProduct="
             <div class='product'>
                 <img class='imgProduct' src='image/exemplo.jpg' alt='Produto'>
-                <div class='infProduct'>Nome:</div>
-                <div class='infProduct'>Descrição:</div>
-                <div class='infProduct'>Preço:</div>
-                <button class='btnProduct'>Detalhes</button>
+                <div class='infoBox'>
+                    <div class='infProduct'>Nome: <span class='info'>Bolo de Cenoura</span></div>
+                    <div class='infProduct'>Descrição: <span class='info'>Bolo</span></div>
+                    <div class='infProduct'>Preço: <span class='info'>R$20,00</span></div>
+                    <button class='btnProduct'>Detalhes</button>
+                </div>
             </div>";
         
         return $demoProduct;
--- projeto_SENAI/functions/cardProductSale.php
+++ projeto_SENAI/functions/cardProductSale.php
@@ -8,13 +8,15 @@
     function cardProduct(){
         $demoProduct="
         <div class='product'>
-            <img class='imgProduct' src='../image/exemplo.jpg' alt='produto'>
-            <div class='infProduct'>Nome:</div>
-            <div class='infProduct'>Descrição:</div>
-            <div class='oldPrice infProduct'>Preço:</div>
-            <div class='newPrice infProduct'>Preço:</div>
-            <button class='btnProduct'>Detalhes</button>
-        </div>";
+                <img class='imgProduct' src='../image/exemplo.jpg' alt='Produto'>
+                <div class='infoBox'>
+                    <div class='infProduct'>Nome: <span class='info'>Bolo de Cenoura</span></div>
+                    <div class='infProduct'>Descrição: <span class='info'>Bolo</span></div>
+                    <div class='oldPrice infProduct'>Preço: <span class='info'>R$20,00</span></div>
+                    <div class='infProduct'>Preço: <span class='info'>R$15,00</span></div>
+                    <button class='btnProduct'>Detalhes</button>
+                </div>
+            </div>";
             
         return $demoProduct;
     }
