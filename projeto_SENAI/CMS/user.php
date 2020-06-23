cda4d9c1c4d25775e84042095a94ab822c520175
Erick Matheus <erick.matheusrs1003@gmail.com>
2020-06-23 07:50:07 -0300

    Atualização do CMS do projeto

--- projeto_SENAI/CMS/user.php
+++ projeto_SENAI/CMS/user.php
@@ -3,48 +3,65 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/user.css">
     <title>CMS - Sistema de Gerenciamento do Site</title>
 </head>
 <body>
+    <div class="modal">
+        <div class="newUser">
+
+        </div>
+    </div>
     <header>
-        <h1 class="subTitle">
-            <span class="title">
-                CMS
-            </span>
-            - Sistema de Gerenciamento do Site.
+        <h1 class="title">
+            CMS - Sistema de Gerenciamento do Site.
         </h1>
         <img class="logo" src="img/bread.png" alt="logo">
     </header>
     <main>
-        <nav>
-            <div class="menu">
-                <div class="option">
-                    <a href="index.php">
-                        <img src="img/admin.png" alt="Adm. Conteudo" class="iconOption">
-                        <h1 class="titleOption">Adm. Conteudo</h1>
-                    </a>
-                </div>
-                <div class="option">
-                    <a href="contact.php">
-                        <img src="img/contact.png" alt="Adm. FaleConosco" class="iconOption">
-                        <h1 class="titleOption">Adm. Fale Conosco</h1>
-                    </a>
-                </div>
-                <div class="option">
-                    <a href="">
-                        <img src="img/user.png" alt="Adm. Usuarios" class="iconOption">
-                        <h1 class="titleOption">Adm. Usuarios</h1>
-                    </a>
-                </div>
+        <nav class="menu">
+            <div class="option">
+                <a href="index.php">
+                    <img src="img/admin.png" alt="Adm. Conteudo" class="iconOption">
+                    <h1 class="titleOption">Adm. Conteudo</h1>
+                </a>
             </div>
-            <div class="logout">
-                <h1 class="message">Bem Vindo, XXXXXXXXX</h1>
-                <input class="btnLogout" type="submit" value="Logout">
+            <div class="option">
+                <a href="contact.php">
+                    <img src="img/contact.png" alt="Adm. FaleConosco" class="iconOption">
+                    <h1 class="titleOption">Adm. Fale Conosco</h1>
+                </a>
+            </div>
+            <div class="option">
+                <a href="">
+                    <img src="img/user.png" alt="Adm. Usuarios" class="iconOption">
+                    <h1 class="titleOption">Adm. Usuarios</h1>
+                </a>
             </div>
         </nav>
         <div class="content">
-            
+            <div class="title">GERENCIAMENTO DE USUARIOS</div>
+            <div class="subtitle">
+                <h2 class="user">USUARIO</h2>
+                <button class="btnNewUser">Novo Usuario</button>
+            </div>
+            <table class="register">
+                <tr class="line">
+                    <td class="collumn">NOME</td>
+                    <td class="collumn">CELULAR</td>
+                    <td class="collumn">PERMISSÃO</td>
+                </tr>
+                <tr class="line">
+                    <td class="collumn"></td>
+                    <td class="collumn"></td>
+                    <td class="collumn"></td>
+                </tr>
+                <tr class="line">
+                    <td class="collumn"></td>
+                    <td class="collumn"></td>
+                    <td class="collumn"></td>
+                </tr>
+            </table>
         </div>
     </main>
     <footer>
--- projeto_SENAI/db/dbPadakaHillValley.sql
+++ projeto_SENAI/db/dbPadakaHillValley.sql
@@ -16,7 +16,26 @@ create table tblContact(
     optionMessage integer not null,
     primary key (idContact)
 );
-
+create table tblConstraint(
+	idConstraint int auto_increment not null,
+    admnisterLevel boolean,
+    operatorLevel boolean,
+    clientLevel boolean,
+    primary key (idConstraint)
+);
+create table tblUser(
+	idUser int auto_increment not null,
+    idConstraint int not null,
+    userName varchar(100) not null,
+    birthDate date not null,
+    email varchar(100) not null,
+    cellphone varchar(15) not null,
+    tellphone varchar(15),
+    primary key (idUser),
+    constraint constaint_user
+    foreign key (idConstraint)
+    references tblConstraint (idConstraint)
+);
 
 drop table tblContact;
 
